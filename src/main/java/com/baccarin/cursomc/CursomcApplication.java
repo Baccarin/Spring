511582912e6c76868ee246a5e69196da8a7c7860
Guilherme Baccarin <guilherme.baccarin@sou.ucpel.edu.br>
2021-07-22 14:39:19 -0300

    criado produto, produtoRep e alimentando o produto no banco

--- src/main/java/com/baccarin/cursomc/CursomcApplication.java
+++ src/main/java/com/baccarin/cursomc/CursomcApplication.java
@@ -8,14 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.baccarin.cursomc.domain.Categoria;
+import com.baccarin.cursomc.domain.Produto;
 import com.baccarin.cursomc.repositories.CategoriaRepository;
-import com.baccarin.cursomc.services.CategoriaService;
+import com.baccarin.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository catRepo;
+	@Autowired
+	private ProdutoRepository produtoRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -28,7 +31,21 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria("Informática");
 		Categoria cat2 = new Categoria("RH");
 		
+		Produto p1 = new Produto("Computador", 1000.0);
+		Produto p2 = new Produto("Televisão", 2300.0);
+		Produto p3 = new Produto("Cadeira", 130.0);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2));
+		cat2.getProdutos().addAll(Arrays.asList(p3));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+		
 		catRepo.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepo.saveAll(Arrays.asList(p1,p2,p3));
+		
 	} 
+	
 
 }
---
+++ src/main/java/com/baccarin/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.baccarin.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.baccarin.cursomc.domain.Produto;
+
+public interface ProdutoRepository  extends JpaRepository<Produto, Integer>{
+
+}
