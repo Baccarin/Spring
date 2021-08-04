be959219e7d039fa3334c284e3698345929078fe
Guilherme Baccarin <guilherme.baccarin@sou.ucpel.edu.br>
2021-08-04 13:51:35 -0300

    serviço que entrega categorias por page e ....

--- src/main/java/com/baccarin/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/baccarin/cursomc/resources/CategoriaResource.java
@@ -3,15 +3,15 @@ package com.baccarin.cursomc.resources;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -47,13 +47,13 @@ public class CategoriaResource {
 		categoria = categoriaService.update(categoria);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> listaCategorias = categoriaService.findAll();
@@ -61,12 +61,26 @@ public class CategoriaResource {
 		listaCategorias.forEach(categoria -> {
 			listaCategoriaDTO.add(new CategoriaDTO(categoria));
 		});
-		
-		//Sugestão do video
-		//List<CategoriaDTO> lista = listaCategorias.stream().map(categoria1 -> new CategoriaDTO(categoria1)).collect(Collectors.toList());
-		
-		return ResponseEntity.ok(listaCategoriaDTO);
 
+		// Sugestão do video
+		// List<CategoriaDTO> lista = listaCategorias.stream().map(categoria1 -> new
+		// CategoriaDTO(categoria1)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listaCategoriaDTO);
+	}
+
+	//URI para chamada do serviço page?linesPerPage=5&orderBy=id&direction=DESC
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy , 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> pageCategoria = categoriaService.findPageCategoria(page, linesPerPage, orderBy, direction);
+		
+		Page<CategoriaDTO> pageCategoriaDTO = pageCategoria.map(categoria -> new
+				 CategoriaDTO(categoria));
+
+		return ResponseEntity.ok().body(pageCategoriaDTO);
 	}
 
 }
